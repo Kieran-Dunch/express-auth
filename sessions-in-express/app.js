ca2526f15c4d7d798b7d84e0a4b3281bd50eb38a
Kieran Dunch <dunch.kieran@gmail.com>
2024-07-08 11:18:36 -0600

    sessions

--- sessions-in-express/app.js
+++ sessions-in-express/app.js
@@ -5,6 +5,14 @@ const session = require("express-session")
 
 const PORT = process.env.PORT || 4001;
 
+app.use(
+  session({
+    secret: "tEsting",
+    resave: false,
+    saveUninitialized: false
+  })
+)
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });

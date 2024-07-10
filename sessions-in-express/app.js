6a2b64e9e0af5501bb120532245bc40c6ed92972
Kieran Dunch <dunch.kieran@gmail.com>
2024-07-10 08:54:03 -0600

    cookie

--- sessions-in-express/app.js
+++ sessions-in-express/app.js
@@ -10,6 +10,7 @@ const store = new session.MemoryStore();
 app.use(
   session({
     secret: "tEsting",
+    cookie: { maxAge: 172800000, secure: true, sameSite: 'none' },
     resave: false,
     saveUninitialized: false,
     store

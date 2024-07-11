fb45e06ac9cb39e800f769d13fc218f8ff3ff7df
Kieran Dunch <dunch.kieran@gmail.com>
2024-07-11 07:57:13 -0600

    login function

--- sessions-in-express/app.js
+++ sessions-in-express/app.js
@@ -17,6 +17,25 @@ app.use(
   })
 )
 
+// logging in function
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.users.findByUsername(username, (err, user) => {
+    if (!user) return res.status(403).json({ msg: "No user found!" });
+    if (user.password === password) {
+      // Add your authenticated property below:
+      req.session.authenticated = true;
+      // Add the user object below:
+      req.session.user = { username, password }
+      // Log the session below:
+      console.log(req.session);
+      res.redirect("/shop");
+    } else {
+      res.status(403).json({ msg: "Bad Credentials" });
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });

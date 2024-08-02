9c3e859e0096179eaa70728d8ac9dde7691c7011
Kieran Dunch <dunch.kieran@gmail.com>
2024-08-02 15:23:42 -0400

    seriaization

--- passport-js/app.js
+++ passport-js/app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
-// Import the passport library below:
 const passport = require("passport");
-// Import the passport-local library below:
 const LocalStrategy = require("passport-local").Strategy;
-
 const session = require("express-session");
 const PORT = process.env.PORT || 5000;
 
@@ -16,18 +13,34 @@ app.use(
   })
 );
 
-// Add the middleware to initialize the passport library below:
 app.use(passport.initialize());
-// Add the middleware to implement a session with passport below:
 app.use(passport.session());
 
-// Add your passport local strategy below:
+// Complete the serializeUser function below:
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+// Complete the deserializeUser function below:
+passport.deserializeUser((id, done) => {
+  db.users.findById(id, function(err, user) {
+    if (err) return done(err);
+    done(null, user);
+  });
+});
+
 passport.use(
   new LocalStrategy(function(username, password, done) {
     db.users.findByUsername(username, (err, user) => {
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (user.password != password) return done(null, false);
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (user.password != password) {
+        return done(null, false);
+      }
       return done(null, user);
     });
   })

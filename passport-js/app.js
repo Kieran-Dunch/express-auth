1dfdf7f61e715e5b797d6c04d5880d08709847fb
Kieran Dunch <dunch.kieran@gmail.com>
2024-08-05 11:01:10 -0400

    register a user

--- passport-js/app.js
+++ passport-js/app.js
@@ -1,55 +1,79 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const store = new session.MemoryStore();
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const session = require("express-session");
-const PORT = process.env.PORT || 5000;
+const db = require("./db");
+const PORT = process.env.PORT || 4001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.use(
   session({
     secret: "secret-key",
     resave: false,
     saveUninitialized: false,
+    store,
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Complete the serializeUser function below:
 passport.serializeUser((user, done) => {
-  done(null, user.id)
+  done(null, user.id);
 });
 
-// Complete the deserializeUser function below:
 passport.deserializeUser((id, done) => {
+  // Look up user id in database.
   db.users.findById(id, function(err, user) {
-    if (err) return done(err);
+    if (err) {
+      return done(err);
+    }
     done(null, user);
   });
 });
 
 passport.use(
-  new LocalStrategy(function(username, password, done) {
-    db.users.findByUsername(username, (err, user) => {
+  new LocalStrategy(function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
       if (err) {
-        return done(err);
+        return cb(err);
       }
       if (!user) {
-        return done(null, false);
+        return cb(null, false);
       }
       if (user.password != password) {
-        return done(null, false);
+        return cb(null, false);
       }
-      return done(null, user);
+      return cb(null, user);
     });
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello from the homepage!");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
+app.get("/profile", (req, res) => {
+  // Pass user object stored in session to the view page:
+  res.render("profile", { user: req.user });
+});
+
+// Add the passport middleware below:
+app.post(
+  "/login",
+  passport.authenticate("local",
+    { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("profile");
+  }
+);
+
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });

a865b6063e64b05091b79524f6637ff1b1c1c933
Kieran Dunch <dunch.kieran@gmail.com>
2024-08-15 11:57:19 -0600

    compare function

--- b-crypt/app.js
+++ b-crypt/app.js
@@ -17,3 +17,19 @@ const passwordHash = async (password, saltRounds) => {
   // if there is an error, return null
   return null;
 };
+
+// Create your password comparison function below:
+// this fucntion receives the plaintext password, and hashes it to compare to the stored hash
+const comparePasswords = async (password, hash) => {
+  try {
+    // compare the plaintext password to the hash
+    const matchFound = await bcrypt.compare(password, hash);
+    // if a match is found, return true
+    return matchFound;
+  } catch (err) {
+    // if there is an error, log it
+    console.log(err);
+  }
+  // if there is an error, return false
+  return false;
+};

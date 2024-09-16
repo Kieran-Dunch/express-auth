762ffccae275c708943d86ac3cdf02281dc8df9b
Kieran Dunch <dunch.kieran@gmail.com>
2024-09-16 11:58:40 -0600

    added oauth2 directory

---
+++ oauth2/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 4001;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/home.html'));
+})
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
+})
+
+app.get('/secret', (req, res) => {
+  res.send('Welcome to the secret area.');
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

577fd72305c68f5dea82aef651beb66849f56979
Kieran Dunch <dunch.kieran@gmail.com>
2024-09-18 08:40:14 -0600

    get client

--- oauth2/model.js
+++ oauth2/model.js
@@ -1,1 +1,13 @@
 let db = require('./db.js');
+
+const getClient = (clientId, clientSecret) => {
+  let confidentialClients = db.confidentialClients.filter((client) => {
+    return client.clientId === clientId && client.clientSecret === clientSecret;
+  });
+  return confidentialClients[0];
+};
+
+
+module.exports = {
+  getClient: getClient
+};
